572e740f17663061bddbe40bf8caaa83ae1fd005
mitchelldesjardins <mitchdes51@gmail.com>
2024-11-27 14:58:37 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mitchell Desjardins on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
